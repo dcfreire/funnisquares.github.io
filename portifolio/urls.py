e0954b08b357592eba7b67a0b91612e8d76755df
toodamfilthy <dcfreire77@gmail.com>
2020-04-23 14:25:36 -0300

    Added titles to single_projects

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path("", include('projects.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
